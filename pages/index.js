7a2f634fdd00bb260f707e5ced45a167b58d875b
GuilhermeLangkammer <65038548+GuilhermeLangkammer@users.noreply.github.com>
2025-02-24 11:39:56 +0000

    fazendo um teste

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-export default function home (){
-  return  <div>  
-    <h1> Bom dia, Boa tarde, Boa noite! Espero que todos estejam bem! </h1>
-    
-    <div> <h3> Minha mensagem vai para todos que já começaram estudar, programação alguma vez e parou, bom eu ja fiz isso bem umas 3 vezes.
-      Todas às vezes por  motivos diferentes. E agora estou aqui no momento mais difícil da minha vida para aprender qualquer coisa, porem não me sinto chateado, nem arrependido, pelo contrário me sinto até mais feliz com isso. Com uma cabeça para melhora para olhar as coisas.
+import { Html } from "next/document";
 
-      Entao não desanima não, você só perde quando para de tentar, e para melhorar mais as coisas, pretendo melhor essa pagina, com layouts, colocar resumos. 
-      e caso vocês animem, o pessoal que está começar como eu. Colocar uma area para dividirmos algumas ideias ou talvez trocar contato. Entrar em qualquer área sem. 
-      Experiencia é muito difícil, talvez possamos começar um projeto, tenho algumas ideias legais e tenho certeza que vocês também. Um abraço! 
-    </h3> </div>
-    
-  </div>
+export default function home() {
+  return (
+    <Html>
+      <div> teste</div>
+    </Html>
+  );
 }
-
